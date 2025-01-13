c7fbeace43ebca1649c260022aa2735eb0ec513a
LordJmw <jameswong5818@gmail.com>
2025-01-13 10:11:46 +0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,275 @@
+const express = require("express")
+const mysql = require("mysql2")
+const fs = require("fs")
+const path = require("path")
+const cors = require("cors")
+const conn = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "",
+    database : "uasdatabase"
+})
+
+conn.connect((err) => {
+    if(err){console.error(err)}
+    else{console.log("connected to database")}
+})
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended : true}))
+app.use(cors())
+
+
+app.get("/",(req,res) => {
+    let sql = `select x.product_name,x.note, x.description, x.quantity, x.product_id, x.store_id, x.isChecked , y.store_image_link, y.store_name, x.product_price, x.product_image_link, x.discount from products as x join store as y
+    on x.store_id = y.store_id`
+    conn.query(sql,(err,results) => {
+        if(err){return res.status(500).json({error : "internal server error"})}
+        res.status(200).json({
+            error : false,
+            status : 200,
+            response : results
+        })
+    })
+})
+
+app.get("/cart",(req,res) => {
+  res.sendFile(path.join(__dirname,"./../public/html/cart.html"), (err) => {
+    if(err){
+      console.log("error reading file")
+    }
+  })
+})
+
+app.get("/check-out",(req,res) => {
+  res.sendFile(path.join(__dirname, "./../public/html/checkout.html"), (err) => {
+    if(err){console.log("error reading file")}
+  })
+})
+
+app.put("/products/:id", (req,res) => {
+    const id = req.params.id
+    console.log(req.body.isChecked,id)
+    const {isChecked, quantity } = req.body
+    let sql = `update products set isChecked = ? , quantity = ? where product_id = ?`
+    conn.query(sql, [isChecked, quantity, id] ,(err,results) => {
+        if(err){return res.status(500).json({error : "internal server error"})}
+        res.status(200).json({
+            error : false,
+            status : 200,
+            response : results
+        })
+    })
+})
+
+
+app.put("/store/:id" , (req,res) => {
+    const id = req.params.id
+    const { isChecked, infos } = req.body
+    let sql = `update products set ` 
+
+    let qtyCases = []
+    let isCheckedCases = []
+    let ids = []
+
+    infos.forEach(info => {
+        qtyCases.push(`when product_id = ${info.id} then ${info.qty}`)
+        isCheckedCases.push(`when product_id = ${info.id} then ${isChecked}`)
+        ids.push(info.id)
+    })
+
+    sql += `quantity = case ${qtyCases.join(" ")} end,`
+    sql += `isChecked = case ${isCheckedCases.join(" ") } end `
+    sql += `where product_id in (${ids.join(",")}) and store_id = ?`
+
+    conn.query(sql,[id],(err,results) => {
+        if(err){
+            return res.status(500).json({error : sql})
+        }
+        if(results.affectedRows == 0){
+          return res.status(404).json({
+            status : 404,
+            info : `can't found store_id ${id} or product_id`
+          })
+        }
+        if(results.affectedRows > 0 && results.affectedRows != ids.length ){
+          return res.status(404).json({
+            status : 404,
+            info : "product_id can't be found"
+          })
+        }
+        res.status(200).json({
+            error : false,
+            status : 200,
+            response : results
+        })
+    })
+})
+
+app.put("/allStores",(req,res)=>{
+    const { isChecked, infos } = req.body
+    let sql = `update products set ` 
+
+    let qtyCases = []
+    let isCheckedCases = []
+    let ids = []
+
+    infos.forEach(info => {
+        qtyCases.push(`when product_id = ${info.id} then ${info.qty}`)
+        isCheckedCases.push(`when product_id = ${info.id} then ${isChecked}`)
+        ids.push(info.id)
+    })
+
+    sql += `quantity = case ${qtyCases.join(" ")} end,`
+    sql += `isChecked = case ${isCheckedCases.join(" ") } end `
+    sql += `where product_id in (${ids.join(",")})`
+
+    conn.query(sql,(err,results) => {
+        if(err){
+            return res.status(500).json({error : sql})
+        }
+        res.status(200).json({
+            error : false,
+            status : 200,
+            response : results
+        })
+    })
+})
+
+app.put("/product/note/:id",(req,res) => {
+  const id = req.params.id
+  const {note} = req.body
+  let sql = `update products set note = ? where product_id = ?`
+  let getSql = `select product_id,note from products where product_id = ?`
+  // if(!note){
+  //   return res.status(400).json({error : "note required", status : 400})
+  // }
+  conn.query(sql, [note,id] ,(err,results) => {
+    if(err){return res.status(500).json({error : "internal server error"})}
+    conn.query(getSql, [id], (err,results1) => {
+      if(err){
+        return res.status(500).json({error : "internal server error"})
+      }
+      res.status(200).json({
+        error : 0,
+        status : 200,
+        response : results1
+      })
+    })
+  })
+})
+
+app.post("/get-product-info", async (req, res) => {
+  let { ids } = req.body;
+  
+  try {
+    let results = [];
+
+    for (let id of ids) {
+      let sql = `SELECT * FROM products AS p JOIN store AS s ON p.store_id = s.store_id WHERE product_id = ?`;
+      
+      const rows = await new Promise((resolve, reject) => {
+        conn.query(sql, [id], (err, results) => {
+          if (err) {
+            console.log("SQL Error:", err);  
+            reject(err);  
+          } else {
+            resolve(results);  
+          }
+        });
+      });
+
+      results.push(...rows);
+    }
+    // console.log(results)
+    res.status(200).json({
+      error: 0,
+      data: results
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/reload", async (req, res) => {
+    const sqlUrl = path.join(__dirname, "./public/assets/databaseProduk.sql");
+    const sqlDump = fs.readFileSync(sqlUrl, 'utf8');
+    // ;split titik koma sebagai end dari sql statement, \s* artinya whitespace(spasi kosong setelah titik koma, $memastikan ; di akhir 
+    // kalimat
+    const queries = sqlDump.split(/;\s*$/m).filter(query => query.trim() !== '');
+  
+    console.log(`SQL Dump loaded from: ${sqlUrl}`);
+  
+    try {
+      // Reset all tables
+      // await conn.promise().query("SET FOREIGN_KEY_CHECKS = 0;");
+      // await conn.promise().query("TRUNCATE TABLE products;");
+      // await conn.promise().query("TRUNCATE TABLE store;"); // Ensure store table is included
+      // await conn.promise().query("SET FOREIGN_KEY_CHECKS = 1;");
+  
+      // Execute SQL dump
+      for (const query of queries) {
+        await conn.promise().query(query);
+        console.log(`Executed: ${query}`);
+      }
+  
+      res.status(200).json({
+        error: false,
+        status: 200,
+        message: "Database reloaded successfully!",
+      });
+    } catch (err) {
+      console.error("Query problem:", err.message);
+      res.status(500).json({
+        error: "Internal server error",
+        details: err.message,
+      });
+    }
+  });
+
+app.delete("/products/:id", (req, res) => {
+    const id = req.params.id;
+  
+    const sql = "DELETE FROM products WHERE product_id = ?";
+    conn.query(sql, [id], (err, results) => {
+      if (err) {
+        console.error("Error deleting product:", err.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+      res.status(200).json({
+        error: false,
+        status: 200,
+        message: "Product deleted successfully",
+        response: results
+      });
+    });
+  });
+  
+app.delete("/products/store/:store_id", (req, res) => {
+  const storeId = req.params.store_id;
+
+  const sql = "DELETE FROM products WHERE store_id = ?";
+  conn.query(sql, [storeId], (err, results) => {
+    if (err) {
+      console.error("Error deleting products by store:", err.message);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    res.status(200).json({
+      error: false,
+      status: 200,
+      message: "Products from the store deleted successfully",
+      response: results
+    });
+  });
+});
+
+
+app.listen(port, ()=>{
+    console.log(`listening to request on port ${port}`)
+})
